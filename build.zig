2375cb94f54e667fa7ad7736a372c480934e8f91
very-blank <aapeli.saarelainen.76@gmail.com>
2025-05-17 12:05:51 +0300

    Update to 0.14.0 and fixed build.zig

--- build.zig
+++ build.zig
@@ -4,22 +4,17 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "ZigQoi",
+    const qoi = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    b.installArtifact(exe);
+    const lib = b.addLibrary(.{
+        .linkage = .static,
+        .name = "Qoi",
+        .root_module = qoi,
+    });
 
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    b.installArtifact(lib);
 }
