a8f191a7708ec554691f60ca1a3894120b1525dd
very-blank <aapeli.saarelainen.76@gmail.com>
2025-06-17 15:51:46 +0300

    fix: createModule -> addModule

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const qoi = b.createModule(.{
+    const qoi = b.addModule("zigqoi", .{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
